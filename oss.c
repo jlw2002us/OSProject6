59fd20208f4eae1a964b346436e71f5c498350ac
jennifer wasson <wasson@hoare7.cs.umsl.edu>
2018-11-16 11:33:33 -0600

    first

---
+++ oss.c
@@ -0,0 +1,192 @@
+ //Jenifer Wasson
+ //Operating Systems
+ //Project 5
+ 
+ //Sources used:  rosettacode.org-banker's algorithm: modified for use in my program
+ 
+#include <semaphore.h>
+  #include <string.h>
+  #include <ctype.h>
+  #include  <sys/shm.h>
+  #include <sys/types.h>
+  #include <sys/wait.h>
+  #include <unistd.h>
+  #include <time.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <signal.h>
+  #include <stdbool.h>
+  #include <time.h>
+  #include <fcntl.h>  
+
+
+
+
+struct Memory {
+     int MaxClaims;
+     int  seconds;
+     int Release;
+     long long int  nanoseconds;
+     int TerminatedProc[2];
+     int processID;
+     int RequestID;
+     int Requests[4];
+     int termNum;
+     };
+
+struct FramesTable{
+     int useBit;
+     int FrameNo;
+     int address;
+     int DirtyBit;
+     };
+struct PageTable{
+     int processID;
+     int pageNo;
+     int frameNo;};
+struct Memory  *shmPTR;
+bool signal_interrupt = false;
+
+
+void  ALARMhandler(int sig)
+{ if(signal_interrupt == false)
+  write (STDOUT_FILENO,"Alarm went off\n",16);
+        signal(SIGALRM, SIG_IGN);
+   signal_interrupt = true;
+  signal(SIGALRM, ALARMhandler); 
+  
+}
+
+ int main(int argc, char* argv[])
+  {
+     struct FramesTable framestables[12];
+     int framesno = 0;
+     struct PageTable pagetable[20];
+     int noProcesses = 0;     
+     FILE *fp;
+     int y;     
+     sem_t *sem;
+     int milliseconds = 0;
+     int childCount = 0;
+     int value = 0;
+     pid_t childID;
+     long int getrand = getpid();
+     signal(SIGALRM, ALARMhandler);
+     int x;
+     int RefPointer = 1;
+     int ShmID;
+     int boundmill = 0;
+     int i,j;
+     
+     for(i = 0; i < 11; i++){
+        framestables[framesno].FrameNo = 0;
+        framestables[framesno].useBit = 0;
+        framestables[framesno].address = 0;
+        framestables[framesno].DirtyBit = 0;}
+  
+      framesno++;
+      
+       
+     key_t ShmKEY;
+     alarm(2); //program can only run 2 seconds;
+      ShmKEY = ftok(".", 'x');
+     ShmID = shmget(ShmKEY, sizeof(struct Memory), IPC_CREAT | 0666);
+     if (ShmID < 0) {
+          printf("*** shmget error (server) ***\n");
+          exit(1);
+     }
+    
+    shmPTR = (struct Memory *) shmat(ShmID, NULL, 0);
+    if (shmPTR == NULL) {
+      printf("*** shmat error (server) ***\n");
+      exit(1);
+    } 
+    shmPTR->Release = -2;
+    shmPTR->seconds = 1;
+    shmPTR->nanoseconds = 0;
+    shmPTR->processID = 0;
+     sem = sem_open ("sem1115", O_CREAT | O_EXCL, 0644, 1);
+      sem_close(sem);
+       
+       fp= fopen("logfile.txt", "a");
+       if (fp == NULL) {
+        fprintf(stderr, "Can't open output file %s!\n",
+          "logfile.txt");
+        exit(1);
+       }               
+        
+      while(1){if(signal_interrupt == true) break;
+          
+          //if(noProcesses > 18) break;
+          if((milliseconds >= boundmill)&&(noProcesses < 1)){
+            childCount++;  noProcesses++; srand(getrand++);
+            
+            srand(getrand++); //fprintf(stderr, "child count is %d\n", shmPTR->processID);
+            value = 1 + (rand()%500); //fork every 1 to 500 milliseconds
+        boundmill = shmPTR->seconds*1000 + (int)(shmPTR->nanoseconds/1000000) + value;
+       
+            if ((childID = fork()) == 0) {
+                char *args[]={"./user",NULL};
+                execvp(args[0],args);
+         
+        
+        perror("child failed to execvp the command");
+        return 1;}} 
+      
+
+           else{ //add time and run second chance algorithm
+             
+    
+           milliseconds = (1000*shmPTR->seconds) + (int)(shmPTR->nanoseconds/1000000);
+          for(y = 0; y < shmPTR->termNum; y++){sem = sem_open("sem1115", 0); sem_wait(sem);
+           //see if children have exited
+              fprintf(stderr, "Process %d is exiting at time %d"":""%lld\n", shmPTR->TerminatedProc[y],shmPTR->seconds, shmPTR->nanoseconds);
+               shmPTR->Release = -2; shmPTR->TerminatedProc[y] = -2;
+               shmPTR->termNum = 0;sem_post(sem); sem_close(sem); childCount--;}
+
+          //fprintf(stderr, "release is %d\n", shmPTR->Release);
+          if(shmPTR->Release != -2){  //if a child is in the critical region
+               sem = sem_open("sem1115", 0); sem_wait(sem); 
+               if(shmPTR->Release == 0)
+                 //fprintf(stderr,"Process %d requests %d at time %d"":""%lld \n", shmPTR->RequestID,shmPTR->Requests[1],shmPTR->seconds, shmPTR->nanoseconds);
+                 if(framesno <= 10){
+                    if(framestables[framesno].address == 0){
+                       framestables[framesno].FrameNo = framesno;
+                       framestables[framesno].address = shmPTR->Requests[1]/1000;
+                       fprintf(stderr,"Frames address is %d\n", framestables[framesno].address);
+                       framestables[framesno].useBit = 1;
+                       framesno++; RefPointer++;}}
+             
+            shmPTR->Release = -2;  sem_post(sem); sem_close(sem);}
+                       
+            long long int nanoseconds = 0;
+           while(nanoseconds < 20000){
+            nanoseconds = nanoseconds + 150;
+
+           }
+            shmPTR->nanoseconds = shmPTR->nanoseconds + nanoseconds;
+           while(shmPTR->nanoseconds >= 1000000000){
+              shmPTR->seconds++;
+              shmPTR->nanoseconds = shmPTR->nanoseconds - 1000000000;}}
+       }
+             do{ if(signal_interrupt == true) break;
+       printf("Clock ticking..\n");
+       sleep(1);
+      }while (true);
+       
+       
+      shmdt((void *) shmPTR);
+       sem_close(sem);
+      
+       sem_unlink("sem1115");
+         
+       fclose(fp);
+       shmctl(ShmID, IPC_RMID, NULL);
+       killpg(getpgid(getpid()), SIGTERM);                //                         printf("Server has removed its shared memory...\n");
+      //                                               printf("Server exits...\n");
+      //                                                    
+                                                               
+                                                                    exit(EXIT_SUCCESS);
+                                                                         
+                                                                           }
+      //
---
+++ user.c
@@ -0,0 +1,81 @@
+#include <string.h>
+  #include <ctype.h>
+  #include  <sys/shm.h>
+  #include <sys/types.h>
+  #include <sys/wait.h>
+  #include <unistd.h>
+  #include <time.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <signal.h>
+  #include <stdbool.h>
+  #include <time.h>
+  #include <semaphore.h>
+
+struct Memory {
+     int MaxClaims;
+     int  seconds;
+     int Release;
+     long long int  nanoseconds;
+     int TerminatedProc[2];
+     int processID;
+     int RequestID;
+     int Requests[4];
+     int termNum;
+};
+
+struct Memory  *shmPTR;
+int main() {
+  key_t ShmKEY;
+  int value = 0;
+   int Requests = 0;
+   int termValue = 0;
+   long int getrand = getpid();
+   int boundmil = 0;
+   int milliseconds = 0;
+   
+   int ShmID;
+          sem_t *sem;
+   //          long long int x = 0;
+     int processID = 0;
+               
+                              ShmKEY = ftok(".",'x');
+                                                   ShmID = shmget(ShmKEY, sizeof(struct Memory), 0666);
+                                                                           if (ShmID < 0){
+                                                                                                        printf("*** shmget error (client) ***\n");
+                                                                                                                                         exit(1);
+                                                                                                                                                                              }
+                                                                                                                                                                                                                       shmPTR = (struct Memory *) shmat(ShmID, NULL, 0);
+                                                                                                                                                                                                                                                                   if(ShmID == -1){
+                                                                                                                                                                                                                                                                                                                    printf("*** shmat error(client) ***\n");
+                                                                                                                                                                                                                                                                                                                                                                          exit(1);
+                                                                                                                                                                                                                                                                                                                                                                                                                                      }
+     shmPTR->processID++;
+     //fprintf(stderr, "Release Value is %d\n", shmPTR->Release);
+     srand(getrand++);
+     value = 1 + (rand()%100); 
+      boundmil =  1000*shmPTR->seconds + (int)(shmPTR->nanoseconds/1000000) + value;
+     processID = shmPTR->processID;
+      
+     termValue = 1000 + value; 
+     while(true){  
+            milliseconds = 1000*shmPTR->seconds + (int)(shmPTR->nanoseconds/1000000);
+     if(shmPTR->Release == -2){ //&&(milliseconds >= boundmil)) {
+                sem = sem_open("sem1115", 0); sem_wait(sem);
+                srand(getrand++); value = 1 + (rand()%32000);   //fprintf(stderr, "Value is %d\n",value);
+                 shmPTR->Requests[1] = value; 
+                 shmPTR->Release = 0; 
+               Requests++;                                                                                                                                                              
+               boundmil =  1070*shmPTR->seconds + (int)(shmPTR->nanoseconds/1000000) + value;
+               shmPTR->RequestID = processID; sem_post(sem); sem_close(sem);
+            
+       if(Requests > termValue)  break;} //if it's rolled the chance to terminate
+         }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+      sem = sem_open("sem1115", 0); sem_wait(sem);
+      //fprintf(stderr,"%s", "child terminated");
+      shmPTR->TerminatedProc[shmPTR->termNum] = processID;
+      shmPTR->termNum++; sem_post(sem); sem_close(sem);
+       shmdt((void *) shmPTR);
+    exit(0);
+}
